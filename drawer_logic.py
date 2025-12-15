3b91bc539dc8015f016aba67210feed87d5fd739
Diegorestrepog1988 <diego.restrepog@est.iudigital.edu.co>
2025-12-14 22:28:52 -0500

    Implement basic drawer logic functions

---
+++ drawer_logic.py
@@ -0,0 +1,19 @@
+drawer_ logic.py
+derecha = 5
+izquierda = 5
+escalones = 0
+
+def    derecha(n):
+  global escalones
+  espaciosIzquierda = " " * derechar * escalones
+  print(espaciosIzquierda + "-" * n + ">")
+
+def abajo(n):
+  global escalones
+  espaciosIzquierda = " " * derechar * escalones
+  print((espaciosIzquierda + (" " * derechar + "|\n")) * n + (espaciosIzquierda + " " * derechar + "V"))
+  escalones = escalones + 1
+
+def reiniciar():
+  global escalones
+  escalones = 0
